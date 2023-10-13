b0904e322299aea3cfd70e9d640ed60c7b602bd9
pablopinheiro01 <pablo-pinheiro@hotmail.com>
2023-10-13 19:15:49 -0300

    add navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,8 @@ android {
 
 dependencies {
 
+    val nav_version = "2.5.3"
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.7.2")
@@ -66,4 +68,7 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    implementation("androidx.navigation:navigation-compose:$nav_version")
+
 }
