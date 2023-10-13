eb2409f2cdbf82e3dfda57bfd7a112a6c545b60c
pablopinheiro01 <pablo-pinheiro@hotmail.com>
2023-10-13 18:20:42 -0300

    baseproject

---
+++ app/src/main/java/br/com/marketlist/model/ProductItem.kt
@@ -0,0 +1,7 @@
+package br.com.marketlist.model
+
+data class ProductItem(
+    val id:Long = 0L,
+    val name:String,
+    val bought:Boolean
+)
---
+++ app/src/main/java/br/com/marketlist/sampledata/SampleListProductItem.kt
@@ -0,0 +1,100 @@
+package br.com.marketlist.sampledata
+
+import br.com.marketlist.model.ProductItem
+
+val sampleFirstList: List<ProductItem> = listOf(
+    ProductItem(
+        name = "Macarrao",
+        bought = false
+    ),
+    ProductItem(
+        name = "Tomate",
+        bought = false
+    ),
+    ProductItem(
+        name = "Cenoura",
+        bought = false
+    ),
+    ProductItem(
+        name = "Cebola",
+        bought = false
+    ),
+    ProductItem(
+        name = "Desinfetante",
+        bought = false
+    ),
+    ProductItem(
+        name = "Candida",
+        bought = false
+    ),
+    ProductItem(
+        name = "Chocolate",
+        bought = false
+    ),
+    ProductItem(
+        name = "Queijo",
+        bought = false
+    ),
+    ProductItem(
+        name = "Pizza",
+        bought = false
+    ),
+    ProductItem(
+        name = "Frango",
+        bought = false
+    ),
+    ProductItem(
+        name = "Tomate",
+        bought = false
+    ),
+    ProductItem(
+        name = "Catchup",
+        bought = false
+    ),
+    ProductItem(
+        name = "Esfiha",
+        bought = false
+    ),
+    ProductItem(
+        name = "Danones",
+        bought = false
+    ),
+    ProductItem(
+        name = "Alface",
+        bought = false
+    ),
+    ProductItem(
+        name = "Picanha",
+        bought = false
+    ),
+    ProductItem(
+        name = "Coxinha",
+        bought = false
+    ),
+    ProductItem(
+        name = "Rice",
+        bought = false
+    ),
+    ProductItem(
+        name = "Beans",
+        bought = false
+    ),
+    ProductItem(
+        name = "Apple",
+        bought = false
+    ),
+    ProductItem(
+        name = "Cenoura",
+        bought = false
+    ),
+    ProductItem(
+        name = "Chocollate",
+        bought = false
+    ),
+    ProductItem(
+        name = "Cakes",
+        bought = false
+    ),
+
+
+)
