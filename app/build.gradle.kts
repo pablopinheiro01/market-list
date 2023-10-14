57e853b55b845e393c84ff3f5626640e27f4ee50
pablopinheiro01 <pablo-pinheiro@hotmail.com>
2023-10-14 17:44:22 -0300

    Add DataStoreModule and import lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,5 +86,8 @@ dependencies {
     kapt("com.google.dagger:hilt-compiler:2.44")
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
 
+    implementation ("androidx.datastore:datastore-preferences:1.0.0")
+
+
 
 }
