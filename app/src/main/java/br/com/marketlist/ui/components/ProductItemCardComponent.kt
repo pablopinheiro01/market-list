02fa9afda40f37dd9301af7e4373422bab482df5
pablopinheiro01 <pablo-pinheiro@hotmail.com>
2023-12-01 23:38:57 -0300

    refactory visualization screen and itens checked

--- app/src/main/java/br/com/marketlist/ui/components/ProductItemCardComponent.kt
+++ app/src/main/java/br/com/marketlist/ui/components/ProductItemCardComponent.kt
@@ -8,16 +8,28 @@ import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.marketlist.data.ProductItem
 import br.com.marketlist.sampledata.sampleFirstList
+import br.com.marketlist.ui.theme.MarketListTheme
+
+@Preview
+@Composable
+fun ProductItemCardComponentPreviewChecked() {
+    ProductItemCardComponent(item = sampleFirstList.first().copy(bought = true))
+}
 
 @Preview
 @Composable
@@ -29,15 +41,25 @@ fun ProductItemCardComponentPreview() {
 fun ProductItemCardComponent(
     modifier: Modifier = Modifier,
     item: ProductItem,
-    onClickItem: (idItemClicked: Long) -> Unit = {}
+    onClickItem: (idItemClicked: Long) -> Unit = {},
 ) {
+
+    val cardColor = if (item.bought) {
+            CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.secondary)
+
+    } else {
+            CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primary)
+
+    }
+
     Card(
         modifier = modifier
             .fillMaxWidth()
             .clip(
                 RoundedCornerShape(12.dp)
             )
-            .heightIn(50.dp)
+            .heightIn(50.dp),
+        colors = cardColor
     ) {
         Row {
             Column {
@@ -54,7 +76,11 @@ fun ProductItemCardComponent(
                     .fillMaxWidth()
                     .align(Alignment.CenterVertically)
             ) {
-                Text(text = item.name)
+                if(item.bought){
+                    Text(text = item.name, textDecoration = TextDecoration.LineThrough)
+                }else{
+                    Text(text = item.name)
+                }
             }
         }
     }
